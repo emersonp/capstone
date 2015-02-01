c8aef6ba17399292d9d9eb735f76be3623b88f78
emersonp <parker.emerson@gmail.com>
2015-02-01 15:42:48 -0800

    Further GUI Developments
    Added more seesaw implementation, still in toy phase.

--- src/capstone/gui.clj
+++ src/capstone/gui.clj
@@ -2,7 +2,16 @@
   (use [seesaw.core])
   (require [capstone.query-interface :as qi]))
 
-(def test-content (apply str (qi/find 'stop_data_2 {:select [:badge :route_number] :conditions {:stop_time 36196}})))
+(def test-content (label (apply str (qi/find 'stop_data_2 {:select [:badge :route_number] :conditions {:stop_time 36196}}))))
+
+(def center-text (flow-panel :align :center :items [(label "Center")]))
+
+(def test-layout (border-panel :north test-content
+                               :center center-text
+                               :east "East"
+                               :west "West"
+                               :vgap 5 :hgap 5 :border 5))
 
 (def primary-window (frame :title "Parker's Trimet Wonderstravaganza"
-                           :content test-content))
+                           :content test-layout
+                           :on-close :exit))
